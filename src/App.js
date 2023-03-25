86452ac32bda5bbe9edbb4533eb76edc85c4f12a
vjz3qz <vjz3qz@virginia.edu>
2023-03-25 19:09:15 -0400

    renamed nav bar

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import Navbar from './components/NavigationBar';
-import './App.css';
+import NavigationBar from './components/NavigationBar';
+import './styles/App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
+        <NavigationBar />
+        <Map />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
