d34be1ccd9eed5357fbdcc994802a8cf5ddd10e0
niketanand-7 <niketanand7@gmail.com>
2023-03-25 13:02:25 -0400

    NewNavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
+
+function NavBar() {
+    return (
+        <div className = "navBar">
+            <div className = "navLogo">
+            <Link to="/">Leftover Love</Link>
+            </div>
+            <div className="navLinks">
+                <Link to="/restaurants">Restaurants</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
