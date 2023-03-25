f199cc601c1647f90fbb46e30e8ce7fa3808d6b1
niketanand-7 <niketanand7@gmail.com>
2023-03-25 12:42:24 -0400

    AddHomeComponent

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to Leftover Love</h1>
+            <p>
+                Leftover Love is a website that connects local restaurants with nearby homeless shelters to donate unused food before it expires. Our goal is
+                to reduce food waste while helping those in need. Join us in making a
+                difference today!
+            </p>
+        </div>
+    );
+}
+
+export default Home;
