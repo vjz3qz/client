d47b93961e68b038491e2071b5074b99be50a19e
darshdevkar <darshdevkar2021@gmail.com>
2023-03-25 21:46:37 -0400

    css work

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/App.css';
 
-function NavBar() {
+function NavigationBar() {
     return (
-        <div className = "navBar">
-            <div className = "navLogo">
+        <div className="nav-bar">
+          <div className="nav-logo">
             <Link to="/">Leftover Love</Link>
-            </div>
-            <div className="navLinks">
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-            </div>
+          </div>
+          <div className="nav-links">
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
         </div>
-    );
+      );
 }
 
-export default NavBar;
+export default NavigationBar;
