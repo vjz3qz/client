f126e8395cb8d330ceaf547e70a6abd54e395d79
vjz3qz <vjz3qz@virginia.edu>
2023-03-25 23:21:00 -0400

    updaed food fomr

--- src/components/FoodForm.js
+++ src/components/FoodForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { Form, Button } from 'react-bootstrap';
 
 const FoodForm = () => {
   const [name, setName] = useState('');
@@ -9,59 +9,74 @@ const FoodForm = () => {
   const [restaurant, setRestaurant] = useState('');
   const [restaurants, setRestaurants] = useState([]);
 
+  // Fetch the list of restaurants on component mount
   useEffect(() => {
-    // Fetch the list of restaurants from the backend API
-    axios.get('/api/restaurants')
-      .then(response => setRestaurants(response.data))
-      .catch(error => console.log(error));
+    fetch('/api/restaurants')
+      .then(response => response.json())
+      .then(data => setRestaurants(data))
+      .catch(error => console.error(error));
   }, []);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Create a new food object with the form data
-    const newFood = {
-      name: name,
-      unit: unit,
-      quantity: quantity,
-      expirationDate: expirationDate,
-      restaurant: restaurant
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {
+      name,
+      unit,
+      quantity,
+      expirationDate,
+      restaurant,
     };
-
-    // Send a POST request to the backend API to create a new food
-    axios.post('/api/foods', newFood)
-      .then(response => console.log(response))
-      .catch(error => console.log(error));
+    const response = await fetch('/api/foods', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    });
+    if (response.ok) {
+      setName('');
+      setUnit('');
+      setQuantity('');
+      setExpirationDate('');
+      setRestaurant('');
+      alert('Food added successfully!');
+    } else {
+      alert('Something went wrong. Please try again.');
+    }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={name} onChange={event => setName(event.target.value)} />
-        </label>
-        <label>
-          Unit:
-          <input type="text" value={unit} onChange={event => setUnit(event.target.value)} />
-        </label>
-        <label>
-          Quantity:
-          <input type="text" value={quantity} onChange={event => setQuantity(event.target.value)} />
-        </label>
-        <label>
-          Expiration Date:
-          <input type="date" value={expirationDate} onChange={event => setExpirationDate(event.target.value)} />
-        </label>
-        <label>
-          Restaurant:
-          <select value={restaurant} onChange={event => setRestaurant(event.target.value)}>
-            <option value="">Select a restaurant</option>
-            {restaurants.map(r => <option key={r._id} value={r._id}>{r.name}</option>)}
-          </select>
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter name" value={name} onChange={(e) => setName(e.target.value)} required />
+      </Form.Group>
+
+      <Form.Group controlId="formUnit">
+        <Form.Label>Unit</Form.Label>
+        <Form.Control type="text" placeholder="Enter unit" value={unit} onChange={(e) => setUnit(e.target.value)} required />
+      </Form.Group>
+
+      <Form.Group controlId="formQuantity">
+        <Form.Label>Quantity</Form.Label>
+        <Form.Control type="text" placeholder="Enter quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)} required />
+      </Form.Group>
+
+      <Form.Group controlId="formExpirationDate">
+        <Form.Label>Expiration Date</Form.Label>
+        <Form.Control type="date" placeholder="Enter expiration date" value={expirationDate} onChange={(e) => setExpirationDate(e.target.value)} required />
+      </Form.Group>
+
+      <Form.Group controlId="formRestaurant">
+        <Form.Label>Restaurant</Form.Label>
+        <Form.Control as="select" value={restaurant} onChange={(e) => setRestaurant(e.target.value)} required>
+          <option value="">-- Select a restaurant --</option>
+          {restaurants.map(restaurant => <option key={restaurant._id} value={restaurant._id}>{restaurant.name}</option>)}
+        </Form.Control>
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
 
