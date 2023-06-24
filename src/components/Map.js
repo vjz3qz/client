0059232b03de8c3602a9f460df058ba3fb4d291a
niketanand-7 <niketanand7@gmail.com>
2023-06-24 11:17:26 -0400

    marker change

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,36 +1,52 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const api_key = process.env.API_KEY;
+const api_key = process.env.REACT_APP_API_KEY;
 
 const Map = () => {
   const defaultProps = {
     center: {
-      lat: 37.7749,
-      lng: -122.4194
+      lat: 38.0355514,
+      lng: -78.5060009
     },
     zoom: 8
   };
-  // Sample marker data
+
   const markers = [
     { lat: 37.7749, lng: -122.4194 },
     { lat: 37.773, lng: -122.421 },
     { lat: 37.772, lng: -122.418 }
   ];
 
-  const Marker = ({ lat, lng }) => <div className="marker" lat={lat} lng={lng} />;
+  const Marker = ({ lat, lng }) => (
+    <div lat={lat} lng={lng}>
+      <img src="/src/styles/market-icon.jpeg" alt="Marker" />
+    </div>
+  );
+
+  const addMarkers = (map, maps) => {
+    markers.forEach(marker => {
+      new maps.Marker({
+        position: { lat: marker.lat, lng: marker.lng },
+        map: map,
+        title: 'Hello World!'
+      });
+    });
+  };
+
+  const handleApiLoaded = (map, maps) => {
+    addMarkers(map, maps);
+  };
 
   return (
-    <div className="map" style={{ height: '500px', width: '100%' }}> {/* Here apply "map" class instead of "map-container" */}
+    <div className="map" style={{ height: '500px', width: '100%' }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: api_key }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
+        onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
       >
-        {/* Add markers or other map components here */}
-        {markers.map(marker => (
-          <Marker key={`${marker.lat}-${marker.lng}`} lat={marker.lat} lng={marker.lng} />
-        ))}
+        {/* You can render additional components or markers here */}
       </GoogleMapReact>
     </div>
   );
